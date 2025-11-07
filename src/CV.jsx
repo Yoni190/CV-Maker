f8d525deb477863306ff4ea403545389016557da
Yoni190 <yfisuhu@gmail.com>
2025-11-07 14:20:04 +0300

    Show Education

--- src/CV.jsx
+++ src/CV.jsx
@@ -1,11 +1,22 @@
-function CV ({formData}) {
+function CV ({formData, educationData}) {
     return (
         <div>
             <h1>CV</h1>
-            <h3>{formData.name}</h3>
-            <p>{formData.email}</p>
-            <p>{formData.phoneNumber}</p>
-            <p>{formData.description}</p>
+            <div className="personalInfo">
+                <h3>{formData.name}</h3>
+                <div className="contact-data" style={{display: 'flex', justifyContent: 'center'}}>
+                    <p>{formData.email} </p> <p> {formData.phoneNumber !== "" && "|"} {formData.phoneNumber}</p>
+                </div>
+                
+                
+                <p>{formData.description}</p>
+            </div>
+
+            <div className="education">
+                <p>{educationData.universityName}</p>
+            </div>
+            
+            
 
         </div>
         
