8e33cec263d06c14f72096038d86c086e8215293
Yoni190 <yfisuhu@gmail.com>
2025-11-12 19:29:36 +0300

    Add remove skill functionality

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -4,21 +4,38 @@ import Info from "./Info"
 function Skill({skillData, setSkillData}) {
 
     const [technicalSkills, setTechnicalSkills] = useState("")
+    const [error, setError] = useState("")
     
     const addSkill = () => {
+        if(technicalSkills === '') {
+            setError('Please enter a skill')
+            return
+        }
         const skills = skillData.technicalSkills
         skills.push(technicalSkills)
         setSkillData({...skillData, technicalSkills: skills})
         setTechnicalSkills("")
+        setError("")
     }
 
     const addSkillKeyboard = (event) => {
         if(event.key === 'Enter') {
+            if(technicalSkills === '') {
+                setError('Please enter a skill')
+                return
+            }
             const skills = skillData.technicalSkills
             skills.push(technicalSkills)
             setSkillData({...skillData, technicalSkills: skills})
             setTechnicalSkills("")
+            setError("")
         }
+    }
+
+    const deleteSkill = (index) => {
+        const skills = skillData.technicalSkills
+        skills.splice(index, 1)
+        setSkillData({...skillData, technicalSkills: skills})
     }
     
     return (
@@ -28,6 +45,22 @@ function Skill({skillData, setSkillData}) {
                 <input className="p-2 border rounded-md flex-1" type="text" name="technical" id="technical" 
                 value={technicalSkills} onChange={(e) => setTechnicalSkills(e.target.value)} placeholder="Ruby, PHP, Node.JS..." onKeyDown={addSkillKeyboard}/>
                 <button className="border px-4 rounded-lg text-white bg-gray-900" onClick={addSkill}>+</button>
+            </div>
+            
+            {error !== '' && <p className="text-red-600 text-left">{error}</p>}
+
+            <div className="technical-skills">
+                <ul>
+                    {skillData.technicalSkills.map((skill, index) => (
+                        <div key={index} className="flex justify-between items-center  mt-2">
+                            <li className="text-left">{skill}</li>
+                            <button onClick={()=>deleteSkill(index)} className="text-white bg-red-500 p-2 rounded-md">Remove</button>
+                        </div>
+                        
+                    ))}
+                        
+                </ul>
+                
             </div>
         </div>
     )
