36ee13d3bffe47f201d61318882738ba97b47703
Yoni190 <yfisuhu@gmail.com>
2025-11-12 17:52:13 +0300

    Add skill when enter is clicked

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -10,13 +10,21 @@ function Skill({skillData, setSkillData}) {
         skills.push(technicalSkills)
         setSkillData({...skillData, technicalSkills: skills})
     }
+
+    const addSkillKeyboard = (event) => {
+        if(event.key === 'Enter') {
+            const skills = skillData.technicalSkills
+            skills.push(technicalSkills)
+            setSkillData({...skillData, technicalSkills: skills})
+        }
+    }
     
     return (
         <div className="flex flex-col">
             <label htmlFor="technical">Technical Skills</label>
             <div className="technical-container flex">
                 <input className="p-2 border rounded-md flex-1" type="text" name="technical" id="technical" 
-                value={technicalSkills} onChange={(e) => setTechnicalSkills(e.target.value)} placeholder="Ruby, PHP, Node.JS..."/>
+                value={technicalSkills} onChange={(e) => setTechnicalSkills(e.target.value)} placeholder="Ruby, PHP, Node.JS..." onKeyDown={addSkillKeyboard}/>
                 <button className="border px-4 rounded-lg text-white bg-gray-900" onClick={addSkill}>+</button>
             </div>
         </div>
