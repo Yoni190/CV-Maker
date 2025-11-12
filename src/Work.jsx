3fd771917f14d6cb642b66a09a92671c2fb86cbe
Yoni190 <yfisuhu@gmail.com>
2025-11-12 15:01:24 +0300

    Add work part

---
+++ src/Work.jsx
@@ -0,0 +1,63 @@
+import Info from "./Info"
+
+function Work({ workData, setWorkData }) {
+    const container = {
+        display: 'flex',
+        flexDirection: 'column',
+        borderRadius: '10px',
+        boxShadow:  '5px 5px 10px rgba(0, 0, 0, 0.2)',
+        padding: '15px',
+        marginTop: '30px'
+    }
+
+    const onChange = (event) => {
+        const { name, value } = event.target
+        setWorkData({...workData, [name]: value})
+    }
+
+    const addBullet = () => {
+        const newArray = workData.bullets
+        newArray[workData.bullets.length] = ""
+        setEducationData({...educationData, bullets: newArray})
+    }
+
+    const deleteBullet = (index) => {
+        const newArray = educationData.bullets
+        newArray.splice(index, 1)
+        setEducationData({...educationData, bullets: newArray})
+    }
+
+    const handleBulletChange = (index, newValue) => {
+        setEducationData(prev => {
+            const updatedBullets = [...prev.bullets]; // copy array
+            updatedBullets[index] = newValue; // update the specific bullet
+            return { ...prev, bullets: updatedBullets }; // update state
+        });
+        };
+
+    
+    return (
+        <div style={container}>
+            <h2 style={{ textAlign: 'left' }}>Work Experience</h2>
+            <Info labelName="Company Name" inputName="companyName" value={workData.companyName} onChangeText={onChange} />
+            <Info labelName="Position" inputName="position" value={workData.position} onChangeText={onChange} />
+            <Info labelName="City" inputName="city" value={workData.city} onChangeText={onChange} />
+            <Info labelName="Start Date" inputName="workStart" value={workData.workStart} onChangeText={onChange} />
+            <Info labelName="End Date" inputName="workEnd" value={workData.workEnd} onChangeText={onChange} />
+            {workData.bullets.map((bullet, index) => (
+                <div key={index}>
+                    <label htmlFor="bullet">Bullet {index+1}</label>
+                    <div className="bullets" style={{ display: 'flex'}}>
+                        <input type="text" name="bullet" id="bullet" value={bullet} style={{ flex: 1 }} onChange={(e) => handleBulletChange(index, e.target.value)}/>
+                        <button onClick={addBullet}>+</button>
+                        {workData.bullets.length > 1 && <button onClick={() => deleteBullet(index)}>-</button>}
+                    </div>
+                </div>
+                
+            ))}
+        </div>
+        
+    )
+}
+
+export default Work
