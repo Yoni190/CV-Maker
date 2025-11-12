82a24a7f1197387e1f535033d59623cc10f366a8
Yoni190 <yfisuhu@gmail.com>
2025-11-12 17:17:59 +0300

    Add technical skills input

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -1,6 +1,18 @@
+import Info from "./Info"
+
 function Skill() {
+    const addSkill = () => {
+        console.log('added')
+    }
+    
     return (
-        <h1>Skills</h1>
+        <div className="flex flex-col">
+            <label htmlFor="technical">Technical Skills</label>
+            <div className="technical-container flex">
+                <input className="p-2 border rounded-md flex-1" type="text" name="technical" id="technical" placeholder="Ruby, PHP, Node.JS..."/>
+                <button className="border px-4 rounded-lg text-white bg-gray-900" onClick={addSkill}>+</button>
+            </div>
+        </div>
     )
 }
 
